8ff96053dc9f4c447498986c6bcc10b76153c8db
Ryan@SOLARIS_WIN_Laptop <ttang@anl.gov>
2025-06-03 16:45:16 -0500

    basic ok

--- GUI.py
+++ GUI.py
@@ -4,9 +4,12 @@ import sys
 import json
 from PyQt6.QtWidgets import (
     QApplication, QWidget, QPushButton, QVBoxLayout, QGridLayout,
-    QGroupBox, QLabel, QHBoxLayout, QFileDialog, QCheckBox, QLineEdit
+    QGroupBox, QLabel,  QFileDialog, QCheckBox, QLineEdit, QDoubleSpinBox,
+    QApplication, QComboBox
 )
 from PyQt6.QtCore import Qt, QTimer
+from PyQt6.QtGui import QCloseEvent
+import time
 
 from Library import Controller
 
@@ -36,12 +39,22 @@ class TargetWheelControl(QWidget):
         self.init_ui()
 
         self.Load_program_setting()
+        self.Connect_Server()
 
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.Update_Status)
-        self.timer.start(1000) # 1 sec
+        self.updateTimeInterval = 1000  # milliseconds
+        self.timer.start(self.updateTimeInterval) # 1 sec
 
-    def __del__(self):
+        self.enableSignals = False #disable signals-slots during initialization
+
+        self.Display_Status()
+
+        self.enableSignals = True  # Enable signals-slots after initial setup
+
+    def closeEvent(self, event: QCloseEvent):
+        self.Save_program_settings()
+        event.accept()  # Optional: confirm you want to close
         print("============= Program Ended.")
 
     def init_ui(self):
@@ -113,10 +126,50 @@ class TargetWheelControl(QWidget):
         status_layout.addWidget(self.connectionStatus, row, 0, 1, 3)
 
         row += 1
-        status_layout.addWidget(QLabel("Encoder Pos : "), row, 0)
+        status_layout.addWidget(QLabel("Encoder Pos. : "), row, 0)
         self.Encoderpos = QLineEdit()
         self.Encoderpos.setReadOnly(True)
         status_layout.addWidget(self.Encoderpos, row, 1, 1, 2)
+
+        row += 1
+        status_layout.addWidget(QLabel("Encoder Rev. : "), row, 0)
+        self.EncoderRev = QLineEdit()
+        self.EncoderRev.setReadOnly(True)
+        status_layout.addWidget(self.EncoderRev, row, 1, 1, 2)
+
+        row += 1
+        status_layout.addWidget(QLabel("Accel. [r/s^2] : "), row, 0)
+        self.spAccel = QDoubleSpinBox()
+        self.spAccel.setDecimals(3)
+        self.spAccel.setSingleStep(0.001)
+        self.spAccel.valueChanged.connect(self.SetAccel)
+        status_layout.addWidget(self.spAccel, row, 1, 1, 2)
+
+        row += 1
+        status_layout.addWidget(QLabel("Speed. [r/s] : "), row, 0)
+        self.spSpeed = QDoubleSpinBox()
+        self.spSpeed.setDecimals(1)
+        self.spSpeed.setSingleStep(0.1)
+        self.spSpeed.valueChanged.connect(self.SetSpeed)
+        status_layout.addWidget(self.spSpeed, row, 1, 1, 2)
+
+        row += 1
+        status_layout.addWidget(QLabel("Deaccel. [r/s^2] : "), row, 0)
+        self.spDeccel = QDoubleSpinBox()
+        self.spDeccel.setDecimals(3)
+        self.spDeccel.setSingleStep(0.001)
+        self.spDeccel.valueChanged.connect(self.SetDeaccel)
+        status_layout.addWidget(self.spDeccel, row, 1, 1, 2)
+
+        row += 1
+        bnSeekHome = QPushButton("Seek Home")
+        status_layout.addWidget(bnSeekHome, row, 0, 1, 3)
+        bnSeekHome.clicked.connect(self.SeekHome)
+
+        row += 1
+        bnZeroEncoderPosition = QPushButton("Zero Encoder Position")
+        status_layout.addWidget(bnZeroEncoderPosition, row, 0, 1, 3)
+        bnZeroEncoderPosition.clicked.connect(self.ZeroEncoderPosition)
 
         row += 1
         self.leSendMsg = QLineEdit()
@@ -129,23 +182,40 @@ class TargetWheelControl(QWidget):
         self.leGetMsg.setReadOnly(True)
         status_layout.addWidget(QLabel("Reply : "), row, 0)
         status_layout.addWidget(self.leGetMsg, row, 1, 1, 2)
+
+
+        # row += 1
+        # bnReset = QPushButton("Reset")
+        # bnReset.clicked.connect(lambda: self.controller.reset())
+        # status_layout.addWidget(bnReset, row, 0, 1, 3)
  
         main_layout.addWidget(status_group, 1, 2, 1, 1)
 
         ########### Spinning Control Group
         spin_group = QGroupBox("Spinning Control")
-        spin_layout = QVBoxLayout()
+        spin_layout = QGridLayout()
         spin_group.setLayout(spin_layout)
 
-        self.spin_label = QLabel("Spin Speed: N/A")
-        spin_layout.addWidget(self.spin_label)
-        
+        row = 0
+        self.spSpinSpeed = QDoubleSpinBox()
+        self.spSpinSpeed.setDecimals(2)
+        self.spSpinSpeed.setSingleStep(0.1)
+        spin_layout.addWidget(QLabel("Spin Speed [r/s] : "), row, 0)
+        spin_layout.addWidget(self.spSpinSpeed, row, 1, 1, 2)
+        self.spSpinSpeed.valueChanged.connect(self.SetSpinSpeed)
+
+        row += 1
+
+
+        row += 1
         bnSpinStart = QPushButton("Start Spin")
-        spin_layout.addWidget(bnSpinStart)
-        bnSpinStart.clicked.connect(lambda: self.controller.send_message("CJ"))
+        spin_layout.addWidget(bnSpinStart, row, 0, 1, 3)
+        bnSpinStart.clicked.connect(lambda: self.controller.startSpin())
+
+        row += 1
         bnSpinStop = QPushButton("Stop Spin")
-        spin_layout.addWidget(bnSpinStop)
-        bnSpinStop.clicked.connect(lambda: self.controller.send_message("SJ"))
+        spin_layout.addWidget(bnSpinStop, row, 0, 1, 3)
+        bnSpinStop.clicked.connect(lambda: self.controller.stopSpin())
         
         main_layout.addWidget(spin_group, 2, 2, 1, 1)
 
@@ -159,19 +229,22 @@ class TargetWheelControl(QWidget):
 
         self.setLayout(main_layout)
 
+
+    #########################################################
     def Load_program_setting(self):
         with open("programSettings.json", "r") as file:
             data = json.load(file)[0]
             self.leIP.setText(data["IP"])
             self.lePort.setText(str(data["Port"]))
-            self.Connect_Server()
 
             self.fileName = data["target_file"]
             self.fileNameLineEdit.setText(self.fileName)
             self.load_targets()
 
     def Save_program_settings(self):
-        pass
+        data = [{"IP": self.leIP.text(), "Port": int(self.lePort.text()), "target_file" : self.fileName}]
+        with open("programSettings.json", "w") as file:
+            json.dump(data, file, indent=2)
 
     def Connect_Server(self):
         self.controller.Connect(self.leIP.text(), int(self.lePort.text()))
@@ -182,11 +255,47 @@ class TargetWheelControl(QWidget):
             self.connectionStatus.setText("Not Connect.")
             self.connectionStatus.setStyleSheet("color : red")
 
-    def Update_Status(self):
-        self.Encoderpos.setText(self.controller.query("RUe1"))
+    def Display_Status(self):
+        if self.controller.connected:
+            self.Encoderpos.setText(f"{self.controller.position}")
+            self.spSpinSpeed.setValue(self.controller.spinSpeed)
+            self.spAccel.setValue(self.controller.accelRate)
+            self.spDeccel.setValue(self.controller.deaccelRate)
+            self.EncoderRev.setText(f"{self.controller.position/8912:.2f} [rev]")
+            self.spSpeed.setValue(self.controller.velocity)
+
+    def Update_Status(self): # see self.updateTimeInterval
+        if self.controller.isSpinning :
+            self.controller.getPosition()
+            self.Encoderpos.setText(f"{self.controller.position}") 
+            self.EncoderRev.setText(f"{self.controller.position/8912:.2f} [rev]")
+
+    def SetSpinSpeed(self):
+        if self.enableSignals:
+            speed = self.spSpinSpeed.value()
+            self.controller.setSpinSpeed(speed)
+            print(f"Spin Speed set to {speed:.2f} [r/s]")
+
+    def SetAccel(self):
+        if self.enableSignals:
+            accel = self.spAccel.value()
+            self.controller.setAccelRate(accel)
+            print(f"Acceleration set to {accel:.3f} [r/s^2]")
+    
+    def SetSpeed(self):
+        if self.enableSignals:
+            speed = self.spSpeed.value()
+            self.controller.setVelocity(speed)
+            print(f"Speed set to {speed:.1f} [r/s]")
+
+    def SetDeaccel(self):
+        if self.enableSignals:
+            deaccel = self.spDeccel.value()
+            self.controller.setDeaccelRate(deaccel)
+            print(f"Deacceleration set to {deaccel:.3f} [r/s^2]")
 
     def Send_Message(self):
-        self.leGetMsg.setText(self.controller.query(self.leSendMsg.text()))
+        self.leGetMsg.setText(self.controller.send_message(self.leSendMsg.text()))
 
     def Target_picked(self, id):
         print(f"Target : {self.target_names[id]}, id : {id}")
@@ -196,6 +305,50 @@ class TargetWheelControl(QWidget):
             self.target_buttons[self.button_clicked_id].setStyleSheet("")
 
         self.button_clicked_id = id
+
+    def SeekHome(self):
+        if self.controller.connected:
+            self.controller.seekHome()
+
+            start_time = time.time()
+            old_position = self.controller.position
+            stableCount  = 0
+            while time.time() - start_time < 10:
+                time.sleep(0.2)  
+                self.controller.getPosition()
+                self.Encoderpos.setText(f"{self.controller.position}") 
+                self.EncoderRev.setText(f"{self.controller.position/8192:.2f} [rev]")
+                QApplication.processEvents()  # Process events to update the UI
+                current_position = self.controller.position
+                print(f"Current position: {current_position}, Old position: {old_position} | count : {stableCount}")
+                if abs(current_position - old_position) < 1:
+                    stableCount += 1
+                    if stableCount >= 5:
+                        print("Home position found.") 
+                        break
+                else:
+                    stableCount = 0
+                old_position = current_position
+            else:
+                print("Home position not found within 10 seconds. ")
+
+            print("Update postion after seeking home.")
+            time.sleep(0.2)  # Wait a bit to ensure the command is processed   
+            self.controller.getPosition()
+            self.Encoderpos.setText(f"{self.controller.position}") 
+            self.EncoderRev.setText(f"{self.controller.position/8912:.2f} [rev]")      
+            QApplication.processEvents()  # Process events to update the UI
+
+    def ZeroEncoderPosition(self):
+        if self.controller.connected:
+            print("Resetting encoder position to 0...")
+            self.controller.setEncoderPosition(0)  # Set the encoder position to 0
+            time.sleep(0.1)  
+            self.controller.getPosition()
+            self.Encoderpos.setText(f"{self.controller.position}") 
+            self.EncoderRev.setText(f"{self.controller.position/8912:.2f} [rev]")      
+            QApplication.processEvents()  # Process events to update the UI
+
 
     def SetPosition(self, id):
         pass
--- Library.py
+++ Library.py
@@ -1,6 +1,7 @@
 import socket
-import threading
+# import threading
 import time
+import math
 
 class Controller():
     def __init__(self):
@@ -10,6 +11,14 @@ class Controller():
         self.sock = None
         self.connected = False
         self.last_message = None
+        self.isSpinning = False
+        self.spinSpeed = 0.0
+        self.commandMode = None
+        self.maxAccel = 0.0
+        self.accelRate = 0.0
+        self.velocity = 0.0
+        self.deaccelRate = 0.0
+        self.position = 0
 
     def __del__(self):
         # Destructor to ensure cleanup
@@ -28,49 +37,150 @@ class Controller():
         except Exception as e:
             print("Connect error:", e)
 
-        # Start the receiving thread
-        if self.connected :
-            receive_thread = threading.Thread(target=self.receive_messages, daemon=True)
-            receive_thread.start()
+        # # Start the receiving thread
+        # if self.connected :
+        #     receive_thread = threading.Thread(target=self.__receive_messages, daemon=True)
+        #     receive_thread.start()
+
+        # self.send_message('RE') 
+        time.sleep(0.1)
+        # self.seekHome()
+        self.getStatus()
+
+    def seekHome(self):
+        if self.connected:
+            print("Seeking home position...")
+            if self.position > 0 :
+                self.send_message('DI-100')
+            elif self.position < 0 :
+                self.send_message('DI100')
+
+            self.send_message('SHX0H') #seek home
             
- 
-    def receive_messages(self):
-        while self.connected:
-            try:
-                # self.sock.settimeout(2)
-                message = b""
-                while True:
-                    chunk = self.sock.recv(1)
-                    if not chunk:
-                        break
-                    message += chunk
-                    if b'\r' in message:
-                        break
-                decoded_message = message[2:-1].decode('utf-8', errors='ignore')
-                print("<- {}".format(decoded_message))
-                self.last_message = decoded_message
-            except Exception as e:
-                print("Receive error:", e)
-                break
+    def setEncoderPosition(self, position):
+        if self.connected and self.isSpinning == False:
+            print(f"Setting encoder position to {position}...")
+            self.send_message(f'EP{position}')
+            
+    def getStatus(self):
+        if self.connected:
+            self.commandMode = self.queryNumber('CM')
+
+            # jogging parameters
+            #check self.spinSpin is number
+            self.spinSpeed = float(self.queryNumber('CS')) # rev/sec
+
+            # point to point movement parameters
+            self.maxAccel = float(self.queryNumber('AM')) # rev/sec/sec, 0.167 - 5461.167
+            self.accelRate = float(self.queryNumber('AC')) # rev/sec/sec
+            self.deaccelRate = float(self.queryNumber('DE')) # rev/sec/sec
+            self.velocity = float(self.queryNumber('VE')) # rev/sec
+
+            self.position = int(self.queryNumber('RUe1')) # encoder position
+
+    def getPosition(self):
+        if self.connected:
+            # haha = self.query('RUe1')
+            # self.position = int(haha) if isinstance(haha, (int)) else None 
+            # return self.position
+            self.position = int(self.queryNumber('RUe1'))
+            return self.position
+
+    def reset(self):
+        if self.connected:
+            print("Resetting controller...")
+            self.send_message('RE')
+            time.sleep(0.1)
+            self.seekHome()
+            self.isSpinning = False
+
+    def setMaxAccel(self, accel):
+        if self.connected:
+            print(f"Setting max acceleration to {accel} rev/sec^2...")
+            self.send_message(f"AM{accel}")
+            self.maxAccel = accel
+
+    def setAccelRate(self, accel):
+        if self.connected:
+            print(f"Setting acceleration rate to {accel} rev/sec^2...")
+            self.send_message(f"AC{accel}")
+            self.accelRate = accel
     
-    def send_message(self, message):
-        if self.connected :
-            try:
-                binary_prefix = b'\x00\x07'
-                binary_suffix = b'\x0d'
-                full_message = binary_prefix + message.encode('utf-8') + binary_suffix
-                self.sock.sendall(full_message)
-                print("->", message)
-            except Exception as e:
-                print("Send error:", e)
+    def setDeaccelRate(self, deaccel):
+        if self.connected:
+            print(f"Setting deacceleration rate to {deaccel} rev/sec^2...")
+            self.send_message(f"DE{deaccel}")
+            self.deaccelRate = deaccel
+    
+    def setVelocity(self, velocity):
+        if self.connected:
+            print(f"Setting velocity to {velocity} rev/sec...")
+            self.send_message(f"VE{velocity}")
+            self.velocity = velocity
+
+    def setSpinSpeed(self, speed):
+        if self.connected:
+            print(f"Setting spin speed to {speed} rev/sec...")
+            self.send_message(f"CS{speed:.1f}")
+            self.spinSpeed = speed    
+
+    def startSpin(self):
+        if self.connected:
+            print("Starting spin...")
+            self.send_message('CJ')
+            self.isSpinning = True
+
+    def stopSpin(self):
+        if self.connected:
+            print("Stopping spin...")
+            self.send_message('SJ')
+            self.isSpinning = False
+
+    def gotoPosition(self, position):
+        if self.connected:
+            print(f"Moving to position {position}...")
+
+            while True: 
+                self.current_position = int(self.queryNumber('RUe1'))  # Get current position
+                print(f"Current position: {self.current_position}")
+                diff = position - self.current_position
+                #estimate the moving time
+                distance = abs(diff) / 8192 # steps to revolutions
+                print(f"Distance to target: {diff:d} steps = {distance:.2f} revolutions")
+                speedUpTime = self.velocity / self.accelRate
+                speedDwTime = self.velocity / self.deaccelRate
+                constSpeedTime = (distance - self.velocity * (speedUpTime + speedDwTime)) / self.velocity if distance > self.velocity * (speedUpTime + speedDwTime) else 0
+                estimatedTime = speedUpTime + constSpeedTime + speedDwTime
+                print(f"Estimated time to reach target: {estimatedTime:.2f} seconds")
+
+                if abs(diff) < 5:
+                    print("Already at the target position.")
+                    break
+                elif diff > 0:
+                    print(f"Moving forward by {diff} steps.")
+                    self.send_message(f"DI{diff}")
+                else:
+                    print(f"Moving backward by {-diff} steps.")
+                    self.send_message(f"DI{-diff}")
+
+                self.send_message('FL') 
+                time.sleep(estimatedTime)  # Wait a bit before checking again
 
     def get_last_message(self):
         return self.last_message
 
-    def query(self, message, timeout=2.0):
+    def queryNumber(self, message, timeout=2.0):
         self.send_message(message)
-        time.sleep(0.1)
-        return self.last_message
+        if self.last_message and '=' in self.last_message:
+            temp = self.last_message.split('=')[1].strip()
+            # Check if temp is a number
+            try:
+                temp = float(temp)
+            except ValueError:
+                return math.nan           
+            return temp
+        else:
+            return math.nan
 
     def disconnect(self):
         """Gracefully disconnect from the server."""
@@ -85,11 +195,74 @@ class Controller():
         self.sock = None
 
 
+    def send_message(self, message):
+        if not self.connected:
+            print("Not connected to server")
+            return None
+        
+        try:
+            # Send message
+            binary_prefix = b'\x00\x07'
+            binary_suffix = b'\x0d'
+            full_message = binary_prefix + message.encode('utf-8') + binary_suffix
+            print("->", message)
+            self.sock.sendall(full_message)
+            
+            # Receive response
+            data = self.sock.recv(1024)  # Buffer size of 1024 bytes
+            decoded_message = data[2:-1].decode('utf-8', errors='ignore')
+            print("<-|{}|".format(decoded_message))
+            self.last_message = decoded_message
+            return self.last_message
+                
+        except Exception as e:
+            print("Send/Receive error:", e)
+            self.connected = False
+            self.last_message = None
+            return None
+        
+        
+    # def __receive_messages(self):
+    #     while self.connected:
+    #         try:
+    #             # self.sock.settimeout(2)
+    #             message = b""
+    #             while True:
+    #                 chunk = self.sock.recv(1)
+    #                 if not chunk:
+    #                     break
+    #                 message += chunk
+    #                 if b'\r' in message:
+    #                     break
+    #             decoded_message = message[2:-1].decode('utf-8', errors='ignore')
+    #             print("<- {}".format(decoded_message))
+    #             self.last_message = decoded_message
+    #         except Exception as e:
+    #             print("Receive error:", e)
+    #             break
+    
+    # def send_message(self, message):
+    #     if self.connected :
+    #         try:
+    #             binary_prefix = b'\x00\x07'
+    #             binary_suffix = b'\x0d'
+    #             full_message = binary_prefix + message.encode('utf-8') + binary_suffix
+    #             self.sock.sendall(full_message)
+    #             print("->", message)
+    #         except Exception as e:
+    #             print("Send error:", e)
+
+
+
+
 # if __name__ == "__main__":
 #     controller = Controller()
 #     controller.Connect('192.168.203.68', 7776)
 
-#     print(controller.query("RUe1"))
+#     # controller.reset()
+#     # controller.seekHome()
+#     # controller.getStatus()
 
+#     print("Current Position:", controller.getPosition())
 #     controller.disconnect()
 
